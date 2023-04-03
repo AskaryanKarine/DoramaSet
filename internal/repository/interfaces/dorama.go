5ed27f382462a390336de000e2a83170774638e5
AskaryanKarine <askariyan.karine@gmail.com>
2023-04-03 23:30:09 +0300

    fixes

--- internal/repository/interfaces/dorama.go
+++ internal/repository/interfaces/dorama.go
@@ -6,6 +6,6 @@ type IDoramaRepo interface {
 	GetList() ([]model.Dorama, error)
 	GetListName(name string) ([]model.Dorama, error)
 	GetDorama(id int) (model.Dorama, error)
-	CreateDorama(record model.Dorama) error
-	UpdateDorama(record model.Dorama) error
+	CreateDorama(dorama model.Dorama) error
+	UpdateDorama(dorama model.Dorama) error
 }
