a3d3444b0778ce5c3cf96a4fb65f854f653febd4
AskaryanKarine <askariyan.karine@gmail.com>
2023-04-11 17:07:40 +0300

    added some function in dorama ans user repository

--- backend/internal/repository/postgres/dorama.go
+++ backend/internal/repository/postgres/dorama.go
@@ -2,11 +2,21 @@ package postgres
 
 import (
 	"DoramaSet/internal/logic/model"
+	"fmt"
 	"gorm.io/gorm"
 )
 
 type DoramaRepo struct {
 	db *gorm.DB
+}
+
+type doramaModel struct {
+	ID          int
+	Name        string
+	Description string
+	ReleaseYear int
+	Status      string
+	Genre       string
 }
 
 func (DoramaRepo) GetList() ([]model.Dorama, error) {
@@ -24,12 +34,30 @@ func (DoramaRepo) GetDorama(id int) (*model.Dorama, error) {
 	panic("implement me")
 }
 
-func (DoramaRepo) CreateDorama(dorama model.Dorama) error {
-	//TODO implement me
-	panic("implement me")
+func (d DoramaRepo) CreateDorama(dorama model.Dorama) (int, error) {
+	m := doramaModel{
+		Name:        dorama.Name,
+		Description: dorama.Description,
+		ReleaseYear: dorama.ReleaseYear,
+		Status:      dorama.Status,
+		Genre:       dorama.Status,
+	}
+	result := d.db.Table("dorama_set.dorama").Create(&m)
+	if result.Error != nil {
+		return -1, fmt.Errorf("db: %w", result.Error)
+	}
+	return m.ID, nil
 }
 
 func (DoramaRepo) UpdateDorama(dorama model.Dorama) error {
 	//TODO implement me
 	panic("implement me")
+}
+
+func (d DoramaRepo) DeleteDorama(dorama model.Dorama) error {
+	result := d.db.Table("dorama_set.dorama").Where("id = ?", dorama.Id).Delete(&doramaModel{})
+	if result.Error != nil {
+		return fmt.Errorf("db: %w", result.Error)
+	}
+	return nil
 }
--- backend/internal/repository/postgres/user.go
+++ backend/internal/repository/postgres/user.go
@@ -1,24 +1,27 @@
 package postgres
 
 import (
+	"DoramaSet/internal/interfaces/repository"
 	"DoramaSet/internal/logic/model"
+	"fmt"
 	"gorm.io/gorm"
 	"time"
 )
 
 type UserRepo struct {
-	db *gorm.DB
+	db      *gorm.DB
+	subRepo repository.ISubscriptionRepo
 }
 
 type userModel struct {
-	Username          string
-	Sub_id            int
-	Password          string
-	Email             string
-	Registration_date time.Time
-	Last_active       time.Time
-	Points            int
-	Is_admin          bool
+	Username         string
+	SubId            int
+	Password         string
+	Email            string
+	RegistrationDate time.Time
+	LastActive       time.Time
+	Points           int
+	IsAdmin          bool
 }
 
 func (u UserRepo) GetUser(username string) (*model.User, error) {
@@ -32,11 +35,36 @@ func (u UserRepo) GetUser(username string) (*model.User, error) {
 }
 
 func (u UserRepo) CreateUser(record model.User) error {
-	//TODO implement me
-	panic("implement me")
+	freeSub, err := u.subRepo.GetSubscriptionByPrice(0)
+	if err != nil {
+		return fmt.Errorf("getSubByPrice: %w", err)
+	}
+	m := userModel{
+		Username:         record.Username,
+		Password:         record.Password,
+		Email:            record.Email,
+		RegistrationDate: record.RegData,
+		LastActive:       record.LastActive,
+		Points:           record.Points,
+		IsAdmin:          record.IsAdmin,
+		SubId:            freeSub.Id,
+	}
+	result := u.db.Table("dorama_set.User").Create(&m)
+	if result.Error != nil {
+		return fmt.Errorf("db: %w", result.Error)
+	}
+	return nil
 }
 
 func (u UserRepo) UpdateUser(record model.User) error {
 	//TODO implement me
 	panic("implement me")
+}
+
+func (u UserRepo) DeleteUser(record model.User) error {
+	result := u.db.Table("dorama_set.User").Where("username = ?", record.Username).Delete(&userModel{})
+	if result.Error != nil {
+		return fmt.Errorf("db: %w", result.Error)
+	}
+	return nil
 }
