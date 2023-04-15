eadc763a95a234988c648607e2384819c0649da8
AskaryanKarine <askariyan.karine@gmail.com>
2023-04-15 22:42:59 +0300

    fix user model

--- backend/internal/logic/model/user.go
+++ backend/internal/logic/model/user.go
@@ -3,13 +3,14 @@ package model
 import "time"
 
 type User struct {
-	Username   string
-	Password   string
-	Email      string
-	RegData    time.Time
-	LastActive time.Time
-	Points     int
-	IsAdmin    bool
-	Sub        *Subscription
-	Collection []List
+	Username      string
+	Password      string
+	Email         string
+	RegData       time.Time
+	LastActive    time.Time
+	LastSubscribe time.Time
+	Points        int
+	IsAdmin       bool
+	Sub           *Subscription
+	Collection    []List
 }
