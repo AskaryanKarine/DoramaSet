ee484b4c616401f380fcd8d27d4467cd7c721f8f
AskaryanKarine <askariyan.karine@gmail.com>
2023-11-28 21:31:46 +0300

    fix container files

--- backend/internal/container/02_insert.sql
+++ backend/internal/container/02_insert.sql
@@ -1,5 +1,5 @@
-insert into dorama_set.Subscription (id, cost, description, duration)
-values (1, 0, 'free', 2592000), (2, 50, 'not free', 2592000);
+insert into dorama_set.Subscription (id, cost, description, duration, access_lvl, name)
+values (1, 0, 'free', 2592000, 1, 'basic'), (2, 50, 'not free', 2592000, 2, 'premium');
 
 insert into dorama_set.User (username, sub_id, password, email, registration_date, last_active, last_subscribe, points)
 values ('test', 1, 'qwerty', 'qwerty@gmail.com', '2023-01-01', '01-01-2023', '2023-01-01', 100),
