343690d55a32a4adc02b14bc113e05511dae119c
AskaryanKarine <askariyan.karine@gmail.com>
2023-12-03 15:27:53 +0300

    finish e2e test

--- backend/internal/e2e/collections_test.go
+++ backend/internal/e2e/collections_test.go
@@ -5,23 +5,72 @@ package e2e
 import (
 	"DoramaSet/internal/handler/apiserver/DTO"
 	"context"
+	json2 "encoding/json"
+	"errors"
+	"fmt"
 	"github.com/ozontech/cute"
 	"github.com/ozontech/cute/asserts/headers"
 	"github.com/ozontech/cute/asserts/json"
-	"log"
 	"net/http"
 	"testing"
-	"time"
 )
 
-func Test_Collection(t *testing.T) {
-	tokens := make(map[string][]string)
+type AsserBody func(body []byte) error
 
+type responseData struct {
+	Data []DTO.List `json:"data"`
+}
+
+func assertPublicLists() AsserBody {
+	return func(body []byte) error {
+		if len(body) == 0 {
+			return errors.New("response is empty")
+		}
+
+		var d responseData
+		err := json2.Unmarshal(body, &d)
+		if err != nil {
+			return fmt.Errorf("unmarshal: %w", err)
+		}
+		if len(d.Data) != 1 {
+			return errors.New("incorrect items in page")
+		}
+
+		list := d.Data[0]
+		if list.Id != 2 {
+			return fmt.Errorf("incorrect id collection, got %d, but expected 2", list.Id)
+		}
+		if list.Name != "test public collection" {
+			return fmt.Errorf("incorrect name collection, got %s, but expected \"test public collection\"", list.Name)
+		}
+		if list.Description != "test public collection" {
+			return fmt.Errorf("incorrect description collection, got %s, but expected \"test public collection\"",
+				list.Description)
+		}
+		if list.Type != "public" {
+			return fmt.Errorf("incorrect type collection, got %s, but expected \"public\"", list.Type)
+		}
+		if list.CreatorName != "user1" {
+			return fmt.Errorf("incorrect creatorName collection, got %s, but expected \"user1\"", list.CreatorName)
+		}
+		if len(list.Doramas) != 1 {
+			return errors.New("incorrect content len in collection")
+		}
+		if list.Doramas[0].Id != 1 {
+			return fmt.Errorf("incorrect items in collection, got id elem %d, but expected 1", list.Doramas[0].Id)
+		}
+
+		return nil
+	}
+}
+
+func Test_Collection(t *testing.T) {
+	myHeaders := make(map[string][]string)
 	baseURI := "http://localhost:8080"
 	cute.NewTestBuilder().
-		Title("Просмотр чужих списков и добавление в избранное").
+		Title("Watching public collection and adding to favourite").
 		Tags("collection").
-		CreateStep("Register sub user").
+		CreateStep("Registered user1").
 		RequestBuilder(
 			cute.WithURI(baseURI+"/auth/registration"),
 			cute.WithMethod(http.MethodPost),
@@ -31,29 +80,20 @@ func Test_Collection(t *testing.T) {
 				Email:    "user1@mail.com",
 			}),
 		).
-		ExpectExecuteTimeout(10*time.Second).
 		ExpectStatus(http.StatusOK).
 		AssertHeaders(
 			headers.Present("Set-Cookie"),
 		).
 		After(func(response *http.Response, errors []error) error {
-			res := response.Header["Set-Cookie"]
-			// res = strings.Split(res[0], ";")
-			tokens["user1"] = res
+			myHeaders["Cookie"] = response.Header["Set-Cookie"]
 			return nil
 		}).
 		NextTest().
-		CreateStep("Create new public list").
-		BeforeExecute(func(request *http.Request) error {
-			log.Println(tokens["user1"])
-			return nil
-		}).
+		CreateStep("Create user1's public list").
 		RequestBuilder(
-			cute.WithHeaders(map[string][]string{
-				"Cookie": tokens["user1"],
-			}),
 			cute.WithURI(baseURI+"/list/"),
 			cute.WithMethod(http.MethodPost),
+			cute.WithHeaders(myHeaders),
 			cute.WithMarshalBody(DTO.List{
 				Name:        "test public collection",
 				Description: "test public collection",
@@ -62,23 +102,73 @@ func Test_Collection(t *testing.T) {
 		).
 		ExpectStatus(http.StatusOK).
 		AssertBody(
-			json.Present("data"),
-			json.Equal("$.name", "test public collection"),
-			json.Equal("$.description", "test public collection"),
-			json.Equal("$.creator_name", "user1"),
+			json.Present("$.data"),
+			json.Equal("$.data.name", "test public collection"),
+			json.Equal("$.data.description", "test public collection"),
+			json.Equal("$.data.creator_name", "user1"),
 		).
 		NextTest().
-		CreateStep("Add dorama in collection").
+		CreateStep("Add dorama in user1's collection").
 		RequestBuilder(
-			cute.WithHeaders(map[string][]string{
-				"Cookie": tokens["user1"],
-			}),
-			cute.WithURI(baseURI+"/list/1"),
+			cute.WithHeaders(myHeaders),
+			cute.WithURI(baseURI+"/list/2"),
 			cute.WithMethod(http.MethodPost),
 			cute.WithMarshalBody(DTO.Id{
 				Id: 1,
 			}),
 		).
 		ExpectStatus(http.StatusOK).
+		NextTest().
+		CreateStep("Register user2").
+		RequestBuilder(
+			cute.WithURI(baseURI+"/auth/registration"),
+			cute.WithMethod(http.MethodPost),
+			cute.WithMarshalBody(DTO.Auth{
+				Login:    "user2",
+				Password: "user123456789",
+				Email:    "user2@mail.com",
+			}),
+		).
+		ExpectStatus(http.StatusOK).
+		AssertHeaders(
+			headers.Present("Set-Cookie"),
+		).
+		After(func(response *http.Response, errors []error) error {
+			res := response.Header["Set-Cookie"]
+			myHeaders["Cookie"] = res
+			return nil
+		}).
+		NextTest().
+		CreateStep("Watch public collection").
+		RequestBuilder(
+			cute.WithURI(baseURI+"/list/public"),
+			cute.WithMethod(http.MethodGet),
+		).
+		ExpectStatus(http.StatusOK).
+		AssertBody(
+			json.Present("$.data"),
+			cute.AssertBody(assertPublicLists()),
+		).
+		NextTest().
+		CreateStep("Add in favourite user2").
+		RequestBuilder(
+			cute.WithURI(baseURI+"/user/favorite"),
+			cute.WithMethod(http.MethodPost),
+			cute.WithMarshalBody(DTO.Id{Id: 2}),
+			cute.WithHeaders(myHeaders),
+		).
+		ExpectStatus(http.StatusOK).
+		NextTest().
+		CreateStep("Get favourite").
+		RequestBuilder(
+			cute.WithURI(baseURI+"/user/favorite"),
+			cute.WithMethod(http.MethodGet),
+			cute.WithHeaders(myHeaders),
+		).
+		ExpectStatus(http.StatusOK).
+		AssertBody(
+			json.Present("$.data"),
+			cute.AssertBody(assertPublicLists()),
+		).
 		ExecuteTest(context.Background(), t)
 }
